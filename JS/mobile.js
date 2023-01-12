c6b34937324458d862ca77407f1259b27f4bfbdf
Himelsu Encoge <himelsu@icloud.com>
2023-01-12 12:28:01 +0200

    Assets update and JS

--- JS/mobile.js
+++ JS/mobile.js
@@ -2,8 +2,14 @@ var log_in_text_01 = window.document.querySelector('button#entrar > a ')
 var log_in_text_02 = window.document.querySelector('button#criar_conta > a ')
 
 log_in_text_01.addEventListener('click', pieta_01)
+log_in_text_02.addEventListener('click', pieta_02)
 
 function pieta_01 ()
-                {
-                    log_in_text_01.style.color = 'gold'
-                }
+                    {
+                        log_in_text_01.style.color = 'gold'
+                    }
+
+function pieta_02 ()
+                    {
+                        log_in_text_01.style.color = 'gold'
+                    }
