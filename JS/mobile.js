dba49fe839fd7fcef33f7c6088dc68651c79e0bf
Himelsu Encoge <himelsu@icloud.com>
2023-01-12 11:00:11 +0200

    Mobile First Main page

---
+++ JS/mobile.js
@@ -0,0 +1,9 @@
+var log_in_text_01 = window.document.querySelector('button#entrar > a ')
+var log_in_text_02 = window.document.querySelector('button#criar_conta > a ')
+
+log_in_text_01.addEventListener('click', pieta_01)
+
+function pieta_01 ()
+                {
+                    log_in_text_01.style.color = 'gold'
+                }
